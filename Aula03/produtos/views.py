b7f33851cb59eabcec12f3eb1375aad10427d523
gegaleski <geovannagaleski24@gmail.com>
2026-08-25 19:29:21 -0300

    Aula05 - ViewSet crud para os itens do backend

--- Aula03/produtos/views.py
+++ Aula03/produtos/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from rest_framework import viewsets # importa o viewset a partir da biblioteca restframework
-from .models import  Produto
-from .serializers import ProdutoSerializer
+from .models import  (Produto, Categoria, Cliente, Pedido, ItemPedido)
+
+from .serializers import (ProdutoSerializer, CategoriaSerializer, ClienteSerializer, PedidoSerializer, ItemPedidoSerializer)
 
 # importndo metodo para exibir uma pagina home
 
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Olá Django ! Aplicações Web 2026 -2 - Aula 03 Loja de Produtos")
+    return HttpResponse("Olá Django ! Aplicações Web 2026 -2 - Aula 05 Loja de Produtos")
 
     # Cria a classe Produtoviewset responsável por permitir fazer o crude
 
@@ -17,5 +18,30 @@ class ProdutoViewSet(viewsets.ModelViewSet):
     serializer_class = ProdutoSerializer
 
 
-# Create your views here.
+# Categoria
+
+class CategoriaViewSet(viewsets.ModelViewSet):
+    queryset = Categoria.objects.all().order_by("id")
+    serializer_class = CategoriaSerializer
+
+
+# Cliente 
+
+class ClienteViewSet(viewsets.ModelViewSet):
+    queryset = Cliente.objects.all().order_by("id")
+    serializer_class = ClienteSerializer
+
+
+# Pedido
+
+class PedoidoViewSet(viewsets.ModelViewSet):
+    queryset = Pedido.objects.all().order_by("id")
+    serializer_class = PedidoSerializer
+
+
+# ItemPedido
+
+class ItemPedidoViewSet(viewsets.ModelViewSet):
+    queryset = ItemPedido.objects.all().order_by("id")
+    serializer_class = ItemPedidoSerializer
 
